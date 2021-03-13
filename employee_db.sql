c37d555d95a79058c0a24c686a7b511a9d6b1cf0
Mikey P <peraram2@gmail.com>
2021-03-12 21:27:46 -0800

    create tables for database

---
+++ employee_db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments (
+    id INTEGER(11) NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE roles (
+    id INTEGER(11) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(15,3) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE employee (
+    id INTEGER(11) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
