9990d547ffa2e737bd4ccbea40fcdb75caa3703f
Mikey P <peraram2@gmail.com>
2021-03-13 10:32:16 -0800

    refractor

--- employee_db.sql
+++ employee_db.sql
@@ -5,19 +5,19 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-    id INTEGER(11) NOT NULL,
+    id INTEGER(11) NOT NULL UNIQUE,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE roles (
-    id INTEGER(11) NOT NULL,
+    id INTEGER(11) NOT NULL UNIQUE,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(15,3) NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE employee (
-    id INTEGER(11) NOT NULL,
+    id INTEGER(11) NOT NULL UNIQUE,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
