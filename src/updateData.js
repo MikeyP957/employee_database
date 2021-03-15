1d6de826ffa1f978413c8758843d14af9278c2c1
Mikey P <peraram2@gmail.com>
2021-03-14 23:14:49 -0700

    testing update data

--- src/updateData.js
+++ src/updateData.js
@@ -1,4 +1,17 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Bongbros#221',
+    database: "employee_db"
+});
+
+
+let departments = ['Developers', 'Managers', 'Human Resources', 'Office'];
+let roles = ['Engineer', 'Accountant', 'Manager', 'Administrative Assistent'];
+let employeeName = ['Malik', 'Theodore', 'Dinh', 'Mariam', 'Juana'];
 
 const updateData = () => {
     inquirer
@@ -29,12 +42,39 @@ const updateData = () => {
         })
 }
 const updateDepartment = () => {
-    console.log('you are updating departments')
+    inquirer
+     .prompt({
+        name: 'departments',
+        type: 'list',
+        message: 'What department would you like to update?',
+        choices: departments,
+     })
+     .then((answer) => {
+         console.log(answer.departments , "this is who you chose")
+     })
 }
 const updateRole = () => {
-    console.log('you are updating Roles')
+    inquirer
+     .prompt({
+        name: 'roles',
+        type: 'list',
+        message: 'What role would you like to update?',
+        choices: roles,
+     })
+     .then((answer) => {
+         console.log(answer.roles , "this is who you chose")
+     })
 }
 const updateEmployee = () => {
-    console.log('you are updating Employees')
+    inquirer
+     .prompt({
+        name: 'names',
+        type: 'list',
+        message: 'Whose would you like to update?',
+        choices: employeeName,
+     })
+     .then((answer) => {
+         console.log(answer.names , "this is who you chose")
+     })
 }
 module.exports = updateData 
